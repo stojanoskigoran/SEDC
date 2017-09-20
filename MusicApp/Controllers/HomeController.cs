1e582726f0d17c26d1614113d37086d71d8c0c30
Bojan Gjorgjioski <bojan.gjorgjioski@outlook.com>
2017-09-20 14:20:05 +0200

    test1

---
+++ MusicApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+
+            //assdasdas
+
+            //asdasdadasaaaaaaaa
+
+            //324234343243
+
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
